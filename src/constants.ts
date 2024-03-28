c9226ccfda8782a6b7f29cfb4ed09e1e3d0c2c86
waki285 <suzuneu@suzuneu.com>
2024-03-28 08:10:42 -0400

    version bump

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const DEV = false;
 
 // 変更必須
-export const VERSION = "0.9.1";
+export const VERSION = "0.10.0";
 
 export const SCRIPT_NAME = "WKSpinner";
 export const SKJ_REQUEST_PAGE_NAME = DEV
